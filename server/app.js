997db8bbc2d0b62261c287ce20406229e7ebc91d
Will Sadler <wsadler@flexion.us>
2015-07-03 11:40:47 -0500

    Initial setup of server

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
-var config = require('./config/environment');
+var config = require('./config/environment/index');
 // Setup server
 var app = express();
 var server = require('http').createServer(app);
---
+++ server/routes.js
@@ -0,0 +1,23 @@
+/**
+ * Main application routes
+ */
+
+'use strict';
+
+var errors = require('./components/errors/index');
+
+module.exports = function(app) {
+
+  // Insert routes below
+  //app.use('/api/epi-search', require('./api/epi-search/index'));
+
+  // All undefined asset or api routes should return a 404
+  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+   .get(errors[404]);
+
+  // All other routes should redirect to the index.html
+  app.route('/*')
+    .get(function(req, res) {
+      res.sendfile(app.get('appPath') + '/index.html');
+    });
+};
