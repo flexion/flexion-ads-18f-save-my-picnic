8784ccab89ad4f87515502ff253fdccecabf44eb
Brice Ruth <bdruth@gmail.com>
2015-07-05 23:06:53 -0500

    Add build: block for CSS gen, fix misc. errors. Fixes #14.

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/gsffda-dev'
+    uri: 'mongodb://localhost/picnicApp-dev'
   },
 
   seedDB: true
--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -5,6 +5,6 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/gsffda-test'
+    uri: 'mongodb://localhost/picnicApp-test'
   }
 };
